f70bdc4dcf6ea6abf2d17b6015efe61dde80d407
Elle Ordoña <eto0507@hotmail.co.uk>
2023-03-24 17:45:05 +0000

    added peep sorting

--- frontend/src/components/Feed/Feed.jsx
+++ frontend/src/components/Feed/Feed.jsx
@@ -4,7 +4,15 @@
 import React from 'react';
 
 const Feed = ({ allPeeps }) => {
-	const peepList = allPeeps.peeps.map((peep) => (
+	// sort the array by date in reverse chronological order
+	let sortedPeeps = allPeeps.peeps
+		.sort((a, b) => {
+			return new Date(a.date).getTime() - new Date(b.date).getTime();
+		})
+		.reverse();
+
+	// map each object in the sortedPeeps into a list item
+	const peepList = sortedPeeps.map((peep) => (
 		<li key={peep.id} className="card text-center w-75 mx-auto">
 			<p className="h5 card-title">{peep.username}</p>
 			<p className="fs-6 card-body">{peep.peepBody}</p>
