d50d02641034ccd9301bbbd7a07d4822d8d219c7
Elle Ordoña <eto0507@hotmail.co.uk>
2023-03-28 20:59:30 +0100

    added username when posting peep when logged in

--- frontend/src/components/PeepForm/PeepForm.jsx
+++ frontend/src/components/PeepForm/PeepForm.jsx
@@ -3,6 +3,7 @@
 // imports
 import PropTypes from 'prop-types';
 import { useState } from 'react';
+import authService from '../utils/auth.service';
 import DateCreated from '../utils/DateCreated';
 
 const PeepForm = ({ submitAction, peep }) => {
@@ -14,14 +15,25 @@ const PeepForm = ({ submitAction, peep }) => {
 	const handleSubmit = (event) => {
 		event.preventDefault();
 
+		// set the users username to the peep if they are logged in
+		const username = currentUser?.username ? currentUser.username : `Unregistered User`;
+
 		submitAction(username, peepBody, date, peep?._id);
 		setUsername(``);
 		setPeepBody(``);
 		setDate(null);
 	};
 
+	const currentUser = authService.getCurrentUser();
+
 	return (
 		<form onSubmit={handleSubmit} className="w-75 text-center mx-auto">
+			{currentUser && (
+				<div className="form-group">
+					<p>Hello {currentUser.username}</p>
+					<input type="text" name="username" hidden placeholder={currentUser.username} value={username} className="form-control" readOnly />
+				</div>
+			)}
 			<div className="form-group">
 				<label htmlFor="peepBody">Write your Peep here:</label>
 				<input type="text" name="peepBody" placeholder="What's Happening?" className="form-control" value={peepBody} onChange={(event) => setPeepBody(event.target.value)} />
